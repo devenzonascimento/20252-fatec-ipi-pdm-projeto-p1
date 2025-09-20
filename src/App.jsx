733e18de263c98ea51cb0d209074d4b376962bf4
Gabriel Vitor <gabriel.v.santos90@gmail.com>
2025-09-20 15:31:44 -0300

    fix: corrigir a grafia do título em index.html e remover o código comentado em App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,8 @@
 function App() {
   return (
     <div className="container mt-2">
-      {/* <div className="row"> */}
-        {/* <div className="col-12"> */}
           <h1 className="text-center">Hello, Lembretes</h1>
         </div>
-      // </div>
-    // </div>
   )
 }
 
