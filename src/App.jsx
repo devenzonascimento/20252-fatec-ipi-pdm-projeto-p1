006f5229cd83769dc1cb8f7f330b5b226080bcc9
Gabriel Vitor <gabriel.v.santos90@gmail.com>
2025-09-20 15:26:59 -0300

    feat(p1): cria componente principal e faz hello

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="container mt-2">
+      {/* <div className="row"> */}
+        {/* <div className="col-12"> */}
+          <h1 className="text-center">Hello, Lembretes</h1>
+        </div>
+      // </div>
+    // </div>
+  )
+}
+
+export default App
