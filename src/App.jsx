4c58a506d764315da61257a064770a2e3eac5774
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2025-09-25 19:44:27 -0300

    feat(p1): cria componente de entrada

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import React from "react";
+import LembreteEntrada from "./LembreteEntrada";
 
-function App() {
-  return (
-    <div className="container mt-2">
-          <h1 className="text-center">Hello, Lembretes</h1>
-        </div>
-  )
+class App extends React.Component {
+  state = {
+    lembretes: [],
+  };
+
+  aoAdicionar = (lembrete) => {
+    this.state.lembretes.push(lembrete)
+    
+    this.setState({ lembretes: this.state.lembretes });
+  };
+
+  render() {
+    return (
+      <div className="container mt-2">
+        <LembreteEntrada aoAdicionar={this.aoAdicionar} />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
---
+++ src/LembreteEntrada.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+class LembreteEntrada extends React.Component {
+  state = {
+    tituloDoLembrete: "",
+  };
+
+  aoDigitar = (e) => {
+    this.setState({
+      tituloDoLembrete: e.target.value,
+    });
+  };
+
+  aoConfirmar = () => {
+    this.props.aoAdicionar(this.state.tituloDoLembrete);
+    this.setState({ tituloDoLembrete: "" });
+  };
+
+  render() {
+    return (
+      <div className="d-sm-flex d-block align-items-center">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Digite seu novo lembrete..."
+          value={this.state.tituloDoLembrete}
+          onChange={this.aoDigitar}
+        />
+
+        <button
+          type="button"
+          className="btn btn-primary col-12 mt-2 col-sm-3 ms-sm-2 mt-sm-0"
+          onClick={this.aoConfirmar}
+        >
+          Adicionar
+        </button>
+      </div>
+    );
+  }
+}
+
+export default LembreteEntrada;
